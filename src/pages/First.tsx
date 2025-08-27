1c0dc4e5b64d332eb81c0974316877a23da4939b
Baran Şahin <sahinbaran611@gmail.com>
2025-08-27 19:21:44 +0300

    Start react native navigation first commit

---
+++ src/pages/First.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+function First() {
+  return (
+
+    <SafeAreaView>
+      <View>
+        <Text>Welcome to the First Page!!!!!</Text>
+      </View>
+      </SafeAreaView>
+ 
+  )
+}
+
+export default First
---
+++ src/pages/Second.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+function Second() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome to the Second Page!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Second
